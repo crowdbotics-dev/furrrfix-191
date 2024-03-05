4ede7ec30d9113389d02208aa82933a89db3040c
Vinay QA test hub <vinay.sharma@crowdbotics.com>
2024-03-05 16:24:42 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "furrrfix-191",
-  url: "https://furrrfix-191.botics.co",
-  api: "https://furrrfix-191.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "furrrfix-191",
+  "url": "https://furrrfix-191.botics.co",
+  "api": "https://furrrfix-191.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "welcome"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
